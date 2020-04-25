b24e6afb38a2ab32be8266cb86feeba85ef5e057
sama007 <smandaliitr@gmail.com>
2020-04-25 07:22:55 +0000

    Deploying to gh-pages from  @ 91e47500d81c02a8693a5fcacb80509d161da4db 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12624dbea67515e08add59f0af00c3e8.js"
+  "/precache-manifest.53f6beed6de71d7cf35199192dbde789.js"
 );
 
 self.addEventListener('message', (event) => {
